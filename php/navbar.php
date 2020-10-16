f35698a082841ad16155f7f3ddc55225434f4c9d
yikwah99 <yikwah99@gmail.com>
2020-10-17 00:06:25 +0800

    include-navbar-icon

--- php/navbar.php
+++ php/navbar.php
@@ -1,1 +1,39 @@
-navbar test
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+  <a class="navbar-brand" href="#"><i class="fas fa-user-nurse mr-2"></i>MyCovidTrail</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <!--Officer Navbar-->
+      <li class="nav-item active">
+        <a class="nav-link" href="officer-manageTestKit.php">Manage Test Kit Stock</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="officer-registerOfficer.php">Register Test Centre Officer</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="officer-registerTestCentre.php">Register Test Centre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="update-view-test-report.php">Update/View Test Report</a>
+      </li>
+      <!--Patient Navbar-->
+      <li class="nav-item">
+        <a class="nav-link" href="patient-viewTest.php">View Test Result</a>
+      </li>
+      <!--Tester Navbar-->
+      <li class="nav-item">
+        <a class="nav-link" href="tester-newTest.php">Record New Test</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="update-view-test-report.php">Update/View Test Report</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link navbar-right" href="login.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- php/officer-registerTestCentre.php
+++ php/officer-registerTestCentre.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>MyCovidTrail|Register Test Centre</title>
+    <title>MyCovidTrail | Register Test Centre</title>
   </head>
   <body>
     <!-- Navbar -->
@@ -19,5 +19,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/165dc4ee52.js" crossorigin="anonymous"></script>
   </body>
 </html>
