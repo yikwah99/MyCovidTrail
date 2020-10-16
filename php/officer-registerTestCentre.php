042e118834139f38c05b9693859c7b6b4b242a1e
Lee199KH <49142302+Lee199KH@users.noreply.github.com>
2020-10-17 02:24:05 +0800

    Trying shit out

--- php/officer-registerTestCentre.php
+++ php/officer-registerTestCentre.php
@@ -21,5 +21,11 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/165dc4ee52.js" crossorigin="anonymous"></script>
+
+    <div class = "Welcome">
+       <!-- Supposed to say "Welcome + UserType and/or Username" Eg. "Welcome Tester Leekeathong" -->
+        <h2 strong> Welcome User! </h2>
+      
+    </div>
   </body>
 </html>
