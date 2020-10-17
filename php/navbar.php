50f06ae7a6c537e4a6dd954fb7d83827860710cf
yikwah99 <yikwah99@gmail.com>
2020-10-17 18:59:31 +0800

    added-navbar-role-filter

--- php/navbar.php
+++ php/navbar.php
@@ -6,6 +6,8 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
+      
+      <?php session_start(); if($_SESSION['role']=="manager"){ ?>
       <!--Officer Navbar-->
       <li class="nav-item active">
         <a class="nav-link" href="officer-manageTestKit.php">Manage Test Kit Stock</a>
@@ -19,10 +21,12 @@
       <li class="nav-item">
         <a class="nav-link" href="update-view-test-report.php">Update/View Test Report</a>
       </li>
+      <?php }else if ($_SESSION['role']=="patient"){ ?>
       <!--Patient Navbar-->
       <li class="nav-item">
         <a class="nav-link" href="patient-viewTest.php">View Test Result</a>
       </li>
+      <?php }else{ ?>
       <!--Tester Navbar-->
       <li class="nav-item">
         <a class="nav-link" href="tester-newTest.php">Record New Test</a>
@@ -30,9 +34,13 @@
       <li class="nav-item">
         <a class="nav-link" href="update-view-test-report.php">Update/View Test Report</a>
       </li>
+      <?php }?>
+      
+      
+      
       
       <li class="nav-item">
-        <a class="nav-link navbar-right" href="login.php">Logout</a>
+        <a class="nav-link navbar-right" href="logout.php">Logout</a>
       </li>
     </ul>
   </div>
