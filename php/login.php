dcb90eb1913b532b60459a0131e80da8f5c30896
yikwah99 <yikwah99@gmail.com>
2020-10-17 18:59:15 +0800

    added-login-php

--- php/login.php
+++ php/login.php
@@ -1,3 +1,55 @@
+<?php
+    
+    session_start();
+    if(isset($_POST['submit']))
+    {
+      include_once("database.php");
+      $check = "";
+      $username=$_POST['username'];
+      $password=$_POST['password'];
+
+      if($username!=''&&$password!='')
+      {
+        //Officer Check
+        $sql = "select * from user,centreOfficer WHERE user.username='".$username."'  AND user.password='".$password."' AND user.username=centreOfficer.username;";
+        $result = mysqli_query($con,$sql);
+        $row = mysqli_fetch_assoc($result);
+        if(!$row){
+          //Patient Check
+          $patientsql = "select * from user,PATIENT WHERE user.username='".$username."'  AND password='".$password."'AND user.username=patient.username;";
+          $patientresult = mysqli_query($con,$patientsql);
+          $patientrow = mysqli_fetch_assoc($patientresult);
+          if(!$patientrow){
+            $check="Wrong Username and Password";
+          }
+          else{
+            $_SESSION['role'] = "patient";
+            header('location:patient-viewTest.php');
+          }
+
+        }
+        else
+        {
+          $_SESSION['username'] =$username;
+          if($row["position"]=="manager"){
+            $_SESSION['role'] = "manager";
+            header('location:officer-registerTestCentre.php');
+          }
+          else{
+            $_SESSION['role'] = "tester";
+            header('location:tester-newTest.php');
+          }
+            
+        }
+        
+
+      }
+      else
+        $check="Username and Password must not be empty!";
+        
+    }
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -13,7 +65,28 @@
   <body>
     <!-- Navbar -->
     <?php include 'navbar.php';?>
-
+    <!-- Main Content -->
+    <div class="container">
+      <div class="card m-5">
+        <div class='card-body'>
+          <h3 class="card-title text-center">Login</h3>
+          <form action="#" method="post">
+            <div class="form-group">
+              <label>Username</label>
+              <input type="text" name="username" class="form-control"  placeholder="">
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input type="password" name="password" class="form-control"  placeholder="">
+            </div>
+            <div class="col-md-12 text-center">
+              <input type="submit" name="submit" value="Login" class="btn btn-primary btn-lg">
+            </div>
+          </form>
+          
+        </div>
+      </div>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
