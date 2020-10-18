aff3af62ce4534f552432769295770d50c5cfc1b
Lee199KH <49142302+Lee199KH@users.noreply.github.com>
2020-10-18 20:33:50 +0800

    Added validation but not functioning yet.

--- php/officer-registerTestCentre.php
+++ php/officer-registerTestCentre.php
@@ -6,22 +6,26 @@ if(!isset($_SESSION))
 include_once("database.php");
 $errormsg="";
 $testcentreResult=mysqli_query($con,"SELECT * FROM testcentre;");
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']) && ($_POST['centreName']!="")){
 
   $testcentreCheck = "select * from testcentre WHERE testcentre.centreName='".$_POST['centreName']."'";
   $testCentreCheckRow = mysqli_num_rows(mysqli_query($con,$testcentreCheck));
 
   if ($testCentreCheckRow>0){
-    $errormsg="TestCentre '".$_POST['centreName']."' already exist!";
     echo '<script>alert("That Test Centre already exists!")</script>';
   }
   else{
     $testCentreInsertSql="INSERT INTO `testcentre` (`centreID`, `centreName`, `Address`, `landline`) VALUES ('".uniqid("TC")."','".$_POST['centreName']."', '".$_POST['address']."', '".$_POST['landline']."')";
     mysqli_query($con,$testCentreInsertSql);
-
+    $_SESSION['message']="New Test Centre Added!";
   }
-}
 
+//data entry validation doesn't work yet.
+/*}
+elseif(($_POST['centreName']="") || ($_POST['landline']="") || ($_POST['address']="")){
+  echo '<script>alert("Please enter all Test Centre Details.")</script>';
+}
+*/
 
 ?>
 <!doctype html>
