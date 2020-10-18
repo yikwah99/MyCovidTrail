0816a887b3124ffea3d6c4b4466e431785a579e6
yikwah99 <yikwah99@gmail.com>
2020-10-18 23:08:22 +0800

    change-navbar-to-be-invisible

--- php/navbar.php
+++ php/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light py-3" style="background:rgba(255,255,255,0.0);">
   
   <a class="navbar-brand" href="#"><i class="fas fa-user-nurse mr-2"></i>MyCovidTrail</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
