a49933800d61fae08ec2a1929b5a7f2c4310debb
yikwah99 <yikwah99@gmail.com>
2020-10-22 04:15:08 +0800

    added-all-session-unset

--- php/logout.php
+++ php/logout.php
@@ -5,5 +5,8 @@ if(!isset($_SESSION))
 } 
 unset($_SESSION["username"]);
 unset($_SESSION["role"]);
+unset($_SESSION["testcentre"]);
+unset($_SESSION["message"]);
+unset($_SESSION["errormessage"]);
 header("Location: login.php");
 ?>
