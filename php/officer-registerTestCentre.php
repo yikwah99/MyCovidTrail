42a94f8322f3132506a9751e3aed71935a2fdba4
yikwah99 <yikwah99@gmail.com>
2020-10-22 05:33:31 +0800

    added-favicon

--- php/officer-registerTestCentre.php
+++ php/officer-registerTestCentre.php
@@ -41,6 +41,7 @@ elseif(($_POST['centreName']="") || ($_POST['landline']="") || ($_POST['address'
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="../img/favicon.ico">
 
     <title>MyCovidTrail | Register Test Centre</title>
 
