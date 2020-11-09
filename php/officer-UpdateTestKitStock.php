a51e2062eccfc2849bb8b2b1a48d3a12f529209c
Lee199KH <49142302+Lee199KH@users.noreply.github.com>
2020-11-09 19:25:26 +0800

    title change

--- php/officer-UpdateTestKitStock.php
+++ php/officer-UpdateTestKitStock.php
@@ -31,7 +31,7 @@ if(isset($_POST['submit'])){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="icon" href="../img/favicon.ico">
-    <title>MyCovidTrail | Add New Test</title>
+    <title>MyCovidTrail | Update Test Kit Stock</title>
   </head>
   <body>
     <!-- Navbar -->
